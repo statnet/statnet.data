784d7ec6213d0f5ffe56759fe259ac67be3dae98
Michal Bojanowski <michal2992@gmail.com>
2021-02-05 23:50:59 +0100

    Add package man page

---
+++ R/statnet.data-package.R
@@ -0,0 +1,24 @@
+#' @section Other packages with network data:
+#' - `ergmharris` (Harris 2013) -- Local Health Department network data set
+#' - `networkdata` (Schoch 2020) -- Big collection of network datasets assembled
+#' from different sources. Not available on CRAN due to its size, but can be
+#' obtained from [GitHub](https://github.com/schochastics/networkdata) Data are
+#' provided as `igraph` objects but can be easily converted to `network` with
+#' [`intergraph::asNetwork()`](https://cran.r-project.org/package=intergraph)
+#'
+#'
+#' @references
+#' Jenine K. Harris (2013). ergmharris: Local Health Department network data
+#' set. R package version 1.0. https://CRAN.R-project.org/package=ergmharris
+#'
+#' David Schoch (2020). networkdata: Repository of Network Datasets. R package
+#' version 0.1.3. https://github.com/schochastics/networkdata
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
