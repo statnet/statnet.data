03b9c6bf9c31b7b09d8f5b3137975e6539b021a0
Michal Bojanowski <michal2992@gmail.com>
2021-02-07 23:43:14 +0100

    Add ergm::molecule

---
+++ R/molecule.R
@@ -0,0 +1,16 @@
+#' Synthetic network with 20 nodes and 28 edges
+#'
+#' This is a synthetic network of 20 nodes that is used as an example within
+#' the `ergm` documentation. It has an interesting elongated shape
+#' - reminencent of a chemical molecule.
+#'
+#' @format Undirected [`network`][network::network] object with 20 nodes and 28
+#'   edges
+#'
+#' @template data
+#'
+#' @docType data
+#' @name molecule
+#' @family undirected networks
+#' @keywords datasets
+"molecule"
