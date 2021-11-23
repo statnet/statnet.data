9b31556f3ce7038dd4e555726d2ffa1ae42e6b96
Michal Bojanowski <michal2992@gmail.com>
2021-11-23 15:08:33 +0100

    Groom man page of 'molecule' data

--- R/molecule.R
+++ R/molecule.R
@@ -1,11 +1,10 @@
 #' Synthetic network with 20 nodes and 28 edges
 #'
 #' This is a synthetic network of 20 nodes that is used as an example within
-#' the `ergm` documentation. It has an interesting elongated shape
-#' - reminencent of a chemical molecule.
+#' the `ergm` documentation. It has an interesting elongated shape - reminiscent
+#' of a chemical molecule.
 #'
-#' @format Undirected [`network`][network::network] object with 20 nodes and 28
-#'   edges
+#' @format `r data_format(molecule)`
 #'
 #' @template data
 #'
