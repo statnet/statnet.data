b3544d2ffdd691999a7bd6475bc93de32845595d
Michal Bojanowski <michal2992@gmail.com>
2021-11-23 03:06:27 +0100

    Document data_format()

--- R/functions.R
+++ R/functions.R
@@ -1,27 +1,33 @@
-# Generate data documentation
-#
-# @description Generate text in Markdown describing a network dataset.
-#
-# @description - `data_format()` generates "Format" section.
-#
-# @param net network; network object to be described
-#
-# @return qwerty
-data_format <- function(net) {
-  stopifnot(inherits(net, "network"))
+#' Generate "Format" section of data documentation
+#'
+#' @description Generates text in Markdown for the "Format" section (without the
+#'   section itself). It is a generic function with methods defined below.
+#'
+#' @param object data object to be described see "Methods" section below for
+#'   available methods
+#'
+#' @return Character scalar with the Markdown text.
+#'
+#' @family data documentation generators
+#'
+data_format <- function(object, ...) UseMethod("data_format")
+
+#' @describeIn data_format Create a paragraph providing information on the size
+#'   of the node set, the edge set and the node and edge attributes.
+data_format.network <- function(object, ...) {
   requireNamespace("network")
 
   out <- character(0)
 
   append(out) <- paste0(
-    if(network::is.directed(net)) "Directed" else "Undirected",
-    if(network::is.bipartite(net)) " bipartite" else NULL,
-    " [`network`][network::network] object with ", network::network.size(net),
+    if(network::is.directed(object)) "Directed" else "Undirected",
+    if(network::is.bipartite(object)) " bipartite" else NULL,
+    " [`network`][network::network] object with ", network::network.size(object),
       " nodes and ",
-    network::network.edgecount(net), " edges."
+    network::network.edgecount(object), " edges."
   )
 
-  vattr <- setdiff(network::list.vertex.attributes(net), c("na", "vertex.names"))
+  vattr <- setdiff(network::list.vertex.attributes(object), c("na", "vertex.names"))
   if(length(vattr) > 0) {
     append(out) <- paste0(
       "Vertex attributes: ",
@@ -30,7 +36,7 @@ data_format <- function(net) {
     )
   }
 
-  eattr <- setdiff(network::list.edge.attributes(net), c("na"))
+  eattr <- setdiff(network::list.edge.attributes(object), c("na"))
   if(length(eattr) > 0) {
     append(out) <- paste0(
       "Edge attributes: ",
