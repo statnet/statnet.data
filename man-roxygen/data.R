6272770a5d77435e8fe0ad759a0a3a8461776ead
Michal Bojanowski <michal2992@gmail.com>
2021-12-03 01:18:51 +0100

    Fix the license link in man pages
    References #1.

--- man-roxygen/data.R
+++ man-roxygen/data.R
@@ -1,7 +1,7 @@
 #'
 #' @section Licenses and Citation: If the section Source of this page does not
 #'   specify otherwise, this data set is protected by the Creative Commons
-#'   License \url{https://creativecommons.org/licenses/by-nc-nd/2.5/}.
+#'   License \url{https://creativecommons.org/licenses/by/4.0/}.
 #'
 #' When publishing results obtained using this data set, the original authors
 #' (see sections Source and/or References) should be cited, along with this
